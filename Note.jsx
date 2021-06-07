442b1e119c50a2071b53e8dac2a89381551148f0
vanitha29 <85499889+vanitha29@users.noreply.github.com>
2021-06-07 19:11:20 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Some Software</h1>
+      <p>Video Editing Software:</p>
+      <ul>
+        <li>Adobe Premiere Pro</li>
+        <li>Final Cut Pro X</li>
+        <li>Adobe Premiere Elements</li>
+        <li>Adobe Premiere Rush</li>
+        <li>Corel VideoStudio Ultimate</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Note;
